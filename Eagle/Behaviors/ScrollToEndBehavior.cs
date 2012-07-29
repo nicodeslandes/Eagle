1c7c104d083935e15fcd37dbf87b944684756b16
Nicolas@Vaio.home <Nicolas@Vaio.home>
2012-07-29 23:12:51 +0100

    Added throttling to the ScrollToEnd behavior, to limit scrollings to at most 1 every 200 ms. Added "fast scrolling" using the list box scroll viewer.
    --HG--
    rename : Eagle/Behavior/ScrollToEndBehavior.cs => Eagle/Behaviors/ScrollToEndBehavior.cs

--- Eagle/Behaviors/ScrollToEndBehavior.cs
+++ Eagle/Behaviors/ScrollToEndBehavior.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Interactivity;
+using System.Windows.Media;
 
-namespace Eagle.Behavior
+namespace Eagle.Behaviors
 {
     public class ScrollToEndBehavior : Behavior<ListBox>
     {
         public static readonly DependencyProperty ScrollToEndOnNewItemsProperty = DependencyProperty.Register("ScrollToEndOnNewItems", typeof(bool), typeof(ScrollToEndBehavior), new UIPropertyMetadata(false, new PropertyChangedCallback(OnScrollToEndOnNewItemsChanged)));
 
+        private ScrollViewer _scrollViewer;
         private static void OnScrollToEndOnNewItemsChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
         {
             ScrollToEndBehavior scrollToEndBehavior = o as ScrollToEndBehavior;
@@ -34,15 +39,12 @@ namespace Eagle.Behavior
             if (enable)
             {
                 this.AssociatedObject.SetBinding(ScrollToEndBehavior.ItemsProperty, new Binding("Items") { RelativeSource = RelativeSource.Self });
-                var items = this.AssociatedObject.Items;
-                if (items != null && items.Count > 0)
-                {
-                    this.AssociatedObject.ScrollIntoView(items[items.Count - 1]);
-                }
+                ScheduleScrollToEnd();
             }
             else
             {
                 SetItems(this.AssociatedObject, null);
+                CancelNextScrollToEnd();
             }
         }
 
@@ -51,14 +53,14 @@ namespace Eagle.Behavior
             get { return (bool)GetValue(ScrollToEndOnNewItemsProperty); }
             set { SetValue(ScrollToEndOnNewItemsProperty, value); }
         }
-        
+
         public static readonly DependencyProperty ItemsProperty = DependencyProperty.RegisterAttached("Items", typeof(ItemCollection), typeof(ScrollToEndBehavior), new UIPropertyMetadata(null, new PropertyChangedCallback(OnItemsChanged)));
 
         public static ItemCollection GetItems(DependencyObject target)
         {
             return (ItemCollection)target.GetValue(ItemsProperty);
         }
-        
+
         public static void SetItems(DependencyObject target, ItemCollection value)
         {
             target.SetValue(ItemsProperty, value);
@@ -88,7 +90,9 @@ namespace Eagle.Behavior
                     {
                         if (ea.Action == NotifyCollectionChangedAction.Add)
                         {
-                            ((ListBox)o).ScrollIntoView(ea.NewItems[0]);
+                            var b = Interaction.GetBehaviors(o)
+                                .OfType<ScrollToEndBehavior>().Single();
+                            b.ScheduleScrollToEnd();
                         }
                     };
                 collection.CollectionChanged += collectionChangedEventHandler;
@@ -109,7 +113,117 @@ namespace Eagle.Behavior
 
         protected override void OnAttached()
         {
+            _scrollViewer = GetScrollViewer(this.AssociatedObject);
+            if (_scrollViewer == null)
+            {
+                EventHandler onLayoutUpdated = null;
+                onLayoutUpdated = (e, s) =>
+                {
+                    _scrollViewer = GetScrollViewer(this.AssociatedObject);
+                    if (_scrollViewer != null) this.AssociatedObject.LayoutUpdated -= onLayoutUpdated;
+                };
+
+                this.AssociatedObject.LayoutUpdated += onLayoutUpdated;
+                this.AssociatedObject.UpdateLayout();
+            }
+
             this.EnableOrDisableScrollToEnd(this.ScrollToEndOnNewItems);
+        }
+
+        private static ScrollViewer GetScrollViewer(DependencyObject element)
+        {
+            var count = VisualTreeHelper.GetChildrenCount(element);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i);
+                if (child is ScrollViewer)
+                {
+                    return (ScrollViewer)child;
+                }
+
+                var scrollViewer = GetScrollViewer(child);
+                if (scrollViewer != null)
+                {
+                    return scrollViewer;
+                }
+            }
+
+            return null;
+        }
+
+        Task _nextScrollTask = null;
+        DateTime _lastScrollTime = DateTime.MinValue;
+        object _scrollSync = new object();
+        static private readonly TimeSpan MinScrollDelay = TimeSpan.FromMilliseconds(200);
+
+        private void ScheduleScrollToEnd()
+        {
+            lock (_scrollSync)
+            {
+                if (_nextScrollTask != null)
+                {
+                    Debug.WriteLine("Scrolling pending; skipping");
+                    return;
+                }
+
+                if (DateTime.Now - _lastScrollTime < MinScrollDelay)
+                {
+                    Debug.WriteLine("Only {0} ms since last scroll; scheduling next scroll in {1} ms", (DateTime.Now - _lastScrollTime).TotalMilliseconds, MinScrollDelay.TotalMilliseconds);
+                    var context = SynchronizationContext.Current;
+                    _nextScrollTask = Task.Delay(MinScrollDelay).ContinueWith(_ => context.Post(a => DoScrollToEnd(), null));
+                    return;
+                }
+            }
+
+            Debug.WriteLine("Scrolling now");
+            DoScrollToEnd();
+        }
+
+        private void DoScrollToEnd(bool checkScrollViewer = true)
+        {
+            if (checkScrollViewer && _scrollViewer == null)
+            {
+                EventHandler handler = null;
+                handler = (s, e) =>
+                {
+                    if (_scrollViewer == null)
+                    {
+                        _scrollViewer = GetScrollViewer(this.AssociatedObject);
+                    }
+
+                    DoScrollToEnd(false);
+                    this.AssociatedObject.LayoutUpdated -= handler;
+                };
+
+                this.AssociatedObject.LayoutUpdated += handler;
+                this.AssociatedObject.UpdateLayout();
+                return;
+            }
+
+            if (_scrollViewer != null)
+            {
+                Debug.WriteLine("Scrolling using scroll viewer");
+                _scrollViewer.ScrollToEnd();
+            }
+            else
+            {
+                var items = this.AssociatedObject.Items;
+                if (items != null && items.Count > 0)
+                {
+                    Debug.WriteLine("Scrolling to end");
+                    this.AssociatedObject.ScrollIntoView(items[items.Count - 1]);
+                }
+            }
+
+            lock (_scrollSync)
+            {
+                _lastScrollTime = DateTime.Now;
+                _nextScrollTask = null;
+            }
+        }
+
+        private void CancelNextScrollToEnd()
+        {
         }
     }
 }
--- Eagle/ViewModel/FileViewModel.cs
+++ Eagle/ViewModel/FileViewModel.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Data;
 using GalaSoft.MvvmLight;
 
 namespace Eagle.ViewModel
