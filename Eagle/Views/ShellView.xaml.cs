b0b5bcc9d25d0e8cc2a789a57cb192aa13381815
Nicolas <none@none>
2013-02-09 00:43:10 +0000

    Code cleanup, and removal from TFS

--- Eagle/Views/ShellView.xaml.cs
+++ Eagle/Views/ShellView.xaml.cs
@@ -1,19 +1,9 @@
-﻿using Eagle.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Interactivity;
-
-namespace Eagle.Views
+﻿namespace Eagle.Views
 {
     /// <summary>
     /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ShellView : Window
+    public partial class ShellView
     {
         public ShellView()
         {
